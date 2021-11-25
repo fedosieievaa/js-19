a1bc81858ee4888d150ab40aad802142dc4526f9
fedosieievaa <nastyabarabakh@gmail.com>
2021-11-25 18:30:00 +0200

    js-19 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
 import './App.css';
+import Contact from './components/Contact';
+import ReactDOM from "react-dom";
+import { useState } from 'react';
 
-function App() {
-    return ( <h1> Just practice to deploy react app on github </h1>);
-}
+const contacts = [{
+    firstName: "Барней",
+    lastName: "Стинсовський",
+    phone: "+380956319521",
+    gender: "male"
+}, {
+    firstName: "Робін",
+    lastName: "Щербатська",
+    phone: "+380931460123",
+    gender: "female"
+}, {
+    firstName: "Анонімний",
+    lastName: "Анонімус",
+    phone: "+380666666666"
+}, {
+    firstName: "Лілія",
+    lastName: "Олдровна",
+    phone: "+380504691254",
+    gender: "female"
+}, {
+    firstName: "Маршен",
+    lastName: "Еріксонян",
+    phone: "+380739432123",
+    gender: "male"
+}, {
+    firstName: "Теодор",
+    lastName: "Мотсбес",
+    phone: "+380956319521",
+    gender: "male"
+}];
 
-export default App;
+function Contacts() {
+    const [searchItem, setSearchItem] = useState("");
+
+    function handleSearchChange(event){
+        setSearchItem(event.target.value);
+    }
+
+    return (
+      <div className="container">
+        <input className="search" type="search" onChange={handleSearchChange}/>
+      { contacts.filter((contacts) => {
+          if(contacts.lastName.toLowerCase().includes(searchItem.toLowerCase())){
+            return contacts;
+          }
+          if(contacts.firstName.toLowerCase().includes(searchItem.toLowerCase())){
+            return contacts;
+          }
+          if(contacts.phone.includes(searchItem)){
+            return contacts;
+          }
+        }).map((contacts) => <Contact contact={contacts}/>) }
+      </div>
+    );
+  }
+  
+ReactDOM.render(<Contacts/>, document.getElementById('root'));
+
+export default Contacts;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Contacts from './App';
 
-ReactDOM.render( < App/> , document.getElementById('root'));
+ReactDOM.render( < Contacts / > , document.getElementById('root'));
